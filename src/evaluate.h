a6da62f5c8e71b173f9ef5f11e13fb3ec5fd5b16
Tony <Tony@secret.com>
2026-05-21 21:43:54 +0200

    use new QAT trained net

--- src/evaluate.h
+++ src/evaluate.h
@@ -33,7 +33,7 @@ namespace Eval {
 // for the build process (profile-build and fishtest) to work. Do not change the
 // name of the macro or the location where this macro is defined, as it is used
 // in the Makefile/Fishtest.
-#define EvalFileDefaultName "nn-a9a192aa8b1b.nnue"
+#define EvalFileDefaultName "nn-71d6d32cb962.nnue"
 
 namespace NNUE {
 class Network;
