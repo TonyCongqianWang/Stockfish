879e196527b35f9897fc13c3629f61651b2c317f
tony <35334345+TonyCongqianWang@users.noreply.github.com>
2026-07-31 04:38:57 +0200

    sim nnueComplexity
    Bench 2569119

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -54,7 +54,6 @@ Value Eval::evaluate(const Eval::NNUE::Network&     network,
     // Blend optimism and eval with nnue complexity
     int nnueComplexity = std::abs(psqt - positional);
     optimism += optimism * i64(nnueComplexity) / 476;
-    nnue -= nnue * i64(nnueComplexity) / 18236;
 
     int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
     int v        = (nnue * i64(77871 + material) + optimism * i64(7191 + material)) / 77871;
