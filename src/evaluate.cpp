74bdd6a84b4c7f885d8023f3989444c0b28abe09
tony <35334345+TonyCongqianWang@users.noreply.github.com>
2026-08-02 01:54:02 +0200

    remove rougue factor
    Bench 2396315

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -86,7 +86,7 @@ Value Eval::scale_evaluation(Value nnue, int optimism, const Position& pos) {
     int mat_multiplier = 7191 + effective_material;
 
     optimism = (optimism * i64(512 + std::abs(se)) * i64(mat_multiplier)) / i64(80000 + material) / 512;
-    int v        = (nnue * i64(80000 + material) + optimism) ;
+    int v        = nnue + optimism;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 199;
