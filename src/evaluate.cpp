e3f03b577c9c2a23417374adbfcb56a9f9f93963
tony <35334345+TonyCongqianWang@users.noreply.github.com>
2026-09-15 14:26:39 +0200

    Remove material scaling from evaluate.cpp (Experiment 1)
    bench 1248683

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -56,8 +56,7 @@ Value Eval::evaluate(const Eval::NNUE::Network&     network,
     optimism += optimism * i64(nnueComplexity) / 476;
     nnue -= nnue * i64(nnueComplexity) / 18236;
 
-    int material = 534 * pos.count<PAWN>() + pos.non_pawn_material();
-    int v        = nnue + (nnue * i64(material) + optimism * i64(7675)) / 91000;
+    int v = nnue + (optimism * i64(7675)) / 91000;
 
     // Damp down the evaluation linearly when shuffling
     v -= v * pos.rule50_count() / 199;
